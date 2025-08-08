96b28643d8cbd85511057ab40ef4d805444eaa8d
Yaretzi Arriaga <yaretzi.a.briones@outlook.com>
2025-08-08 02:25:08 -0600

    Ajustes estilos

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,16 +13,12 @@ const Header = () => {
                     <p>Transformo ideas en interfaces elegantes con Angular. <br />Desarrollo web fullstack con enfoque visual,<br />soluciones ágiles y experiencias que conectan. ✨</p>
                     <div className="btn">
                         {/* Enlace interno que usa HashLink para navegar a una sección de la página */}
-                        <div className="btn-item">
-                            <HashLink to="#portfolio">Proyectos</HashLink>
-                        </div>
+                        <HashLink to="#portfolio">Proyectos</HashLink>
                         
                         {/* Enlace externo a LinkedIn, se mantiene como <a> */}
-                        <div className="btn-item">
-                            <a href="https://www.linkedin.com/in/yaretzi-a-briones/" target="_blank" rel="noopener noreferrer">
-                                LinkedIn
-                            </a>
-                        </div>
+                        <a href="https://www.linkedin.com/in/yaretzi-a-briones/" target="_blank" rel="noopener noreferrer">
+                            LinkedIn
+                        </a>
                     </div>  
                 </div>
                 <div className="img">
