acb82bfad30e0934d47bca69e8528d4aa51d12a7
Yaretzi Arriaga <yaretzi.a.briones@outlook.com>
2025-08-08 01:44:54 -0600

    Ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/mi-portafolio">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
